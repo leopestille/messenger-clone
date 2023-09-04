a34323241aa1b8a69038fa3f75587bd97adf0db2
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-09-04 11:01:21 -0400

    adicionando título na Index

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
 	return (
 		<div
@@ -13,7 +15,25 @@ export default function Home() {
         "
         >
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-
+                <Image
+                    alt="logo"
+                    height="48"
+                    width="48"
+                    className="mx-auto w-auto"
+                    src="/images/logo.png"
+                />
+                <h2
+                className="
+                mt-6
+                text-center
+                text-3xl
+                font-bold
+                tracking-tight
+                text-gray-900
+                "
+                >
+                    Entrar na sua conta
+                </h2>
             </div>
 		</div>
 	);
