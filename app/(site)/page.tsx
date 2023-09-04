aa08e1751bdca9c338e7e12b6da63ddb8ae263ab
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-09-04 10:43:54 -0400

    ajustando css da page Index

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
 	return (
-		<div>
-			<p className="text-sky-500 text-3xl">Essa é a página Index!</p>
+		<div
+        className="
+            flex
+            min-h-full
+            flex-col
+            justify-center
+            py-12
+            sm:px-6
+            lg:px-8
+            bg-gray-100
+        "
+        >
+			
 		</div>
 	);
 }
