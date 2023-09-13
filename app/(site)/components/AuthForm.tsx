d842ba8d4bba2021732394845b685c474ccb23a7
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-09-13 14:05:44 -0400

    criando Context de autenticacao

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,21 +1,31 @@
 "use client";
 
 import axios from "axios";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import { toast } from "react-hot-toast";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
-    const [variant, setVariant] = useState<Variant>("LOGIN");
+    const session = useSession();
+	const router = useRouter();
+	const [variant, setVariant] = useState<Variant>("LOGIN");
     const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		if (session?.status === 'authenticated') {
+			toast.success('Logado com sucesso');
+			router.push('/users')
+		}
+	}, [session?.status, router]);
 
     const togglevariant = useCallback(() => {
 			if (variant === "LOGIN") {
@@ -38,6 +48,7 @@ const AuthForm = () => {
 
         if(variant === 'REGISTER') {
             axios.post('/api/register', data)
+			.then(() => signIn('credentials', data))
 			.catch(() => toast.error('Alguma coisa deu errado!'))
 			.finally(() => setIsLoading(false));
         }
@@ -53,7 +64,8 @@ const AuthForm = () => {
 				}
 
 				if (callback?.ok && !callback?.error) {
-					toast.success('Logado com sucesso')
+					toast.success('Logado com sucesso');
+					router.push('/users');
 				}
 			})
 			.finally(() => setIsLoading(false));
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import ToasterContext from './context/ToasterContext'
+import AuthContext from './context/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,11 +20,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ToasterContext />
-        {children}
-        </body>
-    </html>
-  )
+		<html lang="en">
+			<body className={inter.className}>
+				<AuthContext>
+					<ToasterContext />
+					{children}
+				</AuthContext>
+			</body>
+		</html>
+	);
 }
