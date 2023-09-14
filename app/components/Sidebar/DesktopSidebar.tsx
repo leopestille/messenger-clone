0f923b5069297703167885ede45d28effcd71701
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-09-14 15:34:48 -0400

    criando busca de usuario

--- app/components/Sidebar/DesktopSidebar.tsx
+++ app/components/Sidebar/DesktopSidebar.tsx
@@ -4,9 +4,19 @@ import { useState } from "react";
 import useRoutes from "@/app/hooks/useRoutes";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar = () => {
+import { User } from "@prisma/client";
+
+interface DesktopSidebarProps {
+    currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
+
+    console.log({ currentUser });
     return (
         <div
         className="
